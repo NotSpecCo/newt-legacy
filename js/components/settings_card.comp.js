77e7f9a9efd0a8240c83b801b130722f024cb2ad
Garrett <choorp@gmail.com>
2016-04-15 18:48:51 -0400

    Improved UI

--- js/components/settings_card.comp.js
+++ js/components/settings_card.comp.js
@@ -32,31 +32,43 @@
             }
             
             .settings-row {
-                display: flex;
-                flex-direction: row;
+                // display: flex;
+                // flex-direction: row;
                 padding: 10px 10px;
                 border-bottom: 1px solid #eaeaea;
+            }
+            
+            .main {
+                display: flex;
+                flex-direction: row;
+                // padding: 10px 10px;
+                // border-bottom: 1px solid #eaeaea;
             }
             
             .settings-row .label {
                 flex: 1;
                 font-size: 16px;
             }
+            
+            .settings-row .description {
+                margin-top: 10px;
+                display: none;
+            }
         </style>
         
         <div class='card'>
             <div class='card-title'>Settings</div>
-            <div class='setting-row'>
+            <div class='settings-row'>
                 <div class='main'>
                     <div class='label'>Theme</div>
-                    <select id='prefTheme' name='theme''>
+                    <select id='prefTheme' name='theme'>
                         <option value='light'>Light</option>
                         <option value='dark'>Dark</option>
                         <option value='espresso'>Espresso</option>
                         <option value='custom'>Custom*</option>
                     </select>
                 </div>
-                <div classes='description' id='themeDesc'>
+                <div class='description' id='themeDesc'>
                     * This is an advanced feature! To use a custom theme, write your CSS in a file named "theme-custom.css" and place it in Newt's /css folder.
                 </div>
             </div>
@@ -71,7 +83,7 @@
             this.$theme = this.shadowRoot.querySelector('#prefTheme');
             
             // Add event listeners to save changes to preferences
-            this.$theme.addEventListener('change', this.prefChanged);
+            this.$theme.addEventListener('change', this.prefChanged.bind(this));
             
             // TODO: This is not a good way to handle this. Improve before adding more settings
             if (Newt.prefs.theme == 'dark') {
@@ -80,13 +92,23 @@
                 this.$theme.selectedIndex = 2;
             } else if (Newt.prefs.theme == 'custom') {
                 this.$theme.selectedIndex = 3;
+                this.shadowRoot.querySelector('#themeDesc').style.display = 'block';
             } else {
                 this.$theme.selectedIndex = 0;
             }
         }
         
-        prefChanged(e) {
-            Newt.updatePref(this.name, this.value);
+        prefChanged(ev) {
+            console.log(ev.target);
+            let element = ev.target;
+            
+            if (element.name == 'theme') {
+                console.log(this);
+                let display = element.value == 'custom' ? 'block' : 'none';
+                this.shadowRoot.querySelector('#themeDesc').style.display = display;
+            }
+            
+            Newt.updatePref(element.name, element.value);
         }
 
     }
